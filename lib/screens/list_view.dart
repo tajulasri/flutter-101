6a092331801e700b468aa92e00d99cf32bfd667d
unknown <tajulasri@codexpert.my>
2021-11-09 12:09:13 +0800

    list view

--- lib/screens/list_view.dart
+++ lib/screens/list_view.dart
@@ -1,3 +1,4 @@
+import 'package:asset_kktm/screens/login.dart';
 import 'package:flutter/material.dart';
 
 class ListViewScreen extends StatelessWidget {
@@ -12,6 +13,16 @@ class ListViewScreen extends StatelessWidget {
     {"name": "Ikhwan", "mobile": "01344542657"},
   ];
 
+  //function to navigate to next screen
+  _navigateToDetailScreen(context, contact) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        settings: RouteSettings(arguments: contact),
+        builder: (context) => const LoginScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +30,61 @@ class ListViewScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: Container(
-        child: ListView.builder(
-            itemBuilder: (context, index) => Container(
-                  height: 200,
-                  child: Text(
-                    contacts[index]['mobile'],
+        child: ListView.separated(
+            separatorBuilder: (context, index) => Divider(
+                  color: Colors.blueGrey[200],
+                ),
+            itemBuilder: (context, index) => GestureDetector(
+                  onTap: () => _navigateToDetailScreen(
+                    context,
+                    contacts[index]['name'],
                   ),
+                  child: Container(
+                      height: 60,
+                      margin: EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.call,
+                                color: Colors.blueGrey[500],
+                                size: 30,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10, left: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  contacts[index]['name'],
+                                  style: TextStyle(
+                                    fontSize: 17.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  contacts[index]['mobile'],
+                                  style: TextStyle(
+                                    fontSize: 17.0,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )),
                 ),
             itemCount: contacts.length),
       ),
